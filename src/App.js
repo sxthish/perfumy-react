ac5f8a2d40df5192f560dc2af631bb86b3e82064
sathish kumar <sathishtamizharason@gmail.com>
2024-01-28 14:12:54 +0530

    Updated project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Navbar from './component/Navbar';
+import Search from './component/Search';
+import Product from './component/Product';
+import About from './component/About';
+import Footer from './component/Footer';
+
+function App()
+{
+    return(
+        <div>
+    <Navbar></Navbar>
+    <Search></Search>
+    <Product></Product>
+    <About></About>
+    <Footer></Footer>
+  </div>
+    )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client';
+import "./style.css"
+import App from "./App.js"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+root.render(<App></App>);
+
+
